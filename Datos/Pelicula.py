031e0069770bcf96256a322dc29145485fd5eb20
Alfredo Muñoz Lajara <alfredo_lajara@outlook.es>
2022-11-01 17:15:16 +0100

    Modelos creados

--- Datos/Pelicula.py
+++ Datos/Pelicula.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Pelicula:
     def __init__(self, codigo, titulo, fecha_salida, director, personajes, vehiculos) -> None:
         self.codigo = codigo
@@ -6,3 +9,7 @@ class Pelicula:
         self.director = director
         self.personajes = personajes
         self.vehiculos = vehiculos
+
+    def __str__(self) -> str:
+        return f"Codigo: {self.codigo}, Titulo: {self.titulo}, Fecha de salida: {self.fecha_salida}, Director: {self.director}, Personajes: {self.personajes}, Vehiculos: {self.vehiculos}"
+
