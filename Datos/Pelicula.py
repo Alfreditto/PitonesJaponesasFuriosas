b1caaea1bed709b236e8ee51f855e87fe18d193c
Alfredo Lajara <alfredo_lajara@outlook.es>
2022-11-01 15:47:36 +0100

    Initial commit

---
+++ Datos/Pelicula.py
@@ -0,0 +1,8 @@
+class Pelicula:
+    def __init__(self, codigo, titulo, fecha_salida, director, personajes, vehiculos) -> None:
+        self.codigo = codigo
+        self.titulo = titulo
+        self.fecha_salida = fecha_salida
+        self.director = director
+        self.personajes = personajes
+        self.vehiculos = vehiculos
